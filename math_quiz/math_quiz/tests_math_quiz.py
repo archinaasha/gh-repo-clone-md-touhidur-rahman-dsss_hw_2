3acaf6a9e8e82ef07e73c0ae19069d352b3e9076
md touhidur rahman <touhid129@gmail.com>
2024-11-09 23:19:35 +0100

    Add unit tests for function_A, function_B, and function_C in tests_math_quiz.py

--- math_quiz/math_quiz/tests_math_quiz.py
+++ math_quiz/math_quiz/tests_math_quiz.py
@@ -6,25 +6,30 @@ class TestMathGame(unittest.TestCase):
 
     def test_function_A(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
+        min = 1
+        max = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+            rand_num = function_A(min, max)
+            self.assertTrue(min <= rand_num <= max)
 
     def test_function_B(self):
         # TODO
+        result = function_B()
+        self.assertIn(result, ['+', '-', '*'])
         pass
 
     def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (10, 2, '-', "10 - 2", 8),
+            (5, 5, '*', "5 * 5", 25)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+                question, answer = function_C(num1, num2, operator)
+                self.assertEqual(question, expected_problem)
+                self.assertEqual(answer, expected_answer)
+        pass
 
 if __name__ == "__main__":
     unittest.main()
